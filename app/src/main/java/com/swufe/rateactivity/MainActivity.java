1204d09a021f0e8a81315817906c1b2081bb7227
Juicy2000 <1076912982@qq.com>
2020-06-04 13:17:40 +0800

    Initial commit

---
+++ app/src/main/java/com/swufe/rateactivity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.swufe.rateactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
+    EditText rmb;
+    TextView show;
+    
+}
