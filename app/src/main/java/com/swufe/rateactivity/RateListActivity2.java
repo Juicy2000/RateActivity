6f0edae4374d7407bb4ee5a9088a5a3b573843f7
Juicy2000 <1076912982@qq.com>
2020-06-04 19:30:33 +0800

    第5单元：汇率计算 第6单元：完成汇率配置页面 第7单元：完成汇率保存 第8单元：使用多线程获取网络数据 第9单元：解析汇率 第10单元：设置汇率更新频率 第11单元：列表的显示 第12单元：自定义列表 第13单元：列表事件处理

--- app/src/main/java/com/swufe/rateactivity/RateListActivity2.java
+++ app/src/main/java/com/swufe/rateactivity/RateListActivity2.java
@@ -1,19 +1,76 @@
-package com.swufe.rateactivity;
+package com.swufe.rateactivity.;
 
-public class RateListActivity2 {
-    private void initListView() {
-        listItems = new ArrayList<HashMap<String, String>>();
-        for (int i = 0; i < 10; i++) {
-            HashMap<String, String> map = new HashMap<String, String>();
-            map.put("ItemTitle", "Rate： " + i); // 标题文字
-            map.put("ItemDetail", "detail" + i); // 详情描述
-            listItems.add(map);
-        }
-        // 生成适配器的Item和动态数组对应的元素
-        listItemAdapter = new SimpleAdapter(this, listItems, // listItems数据源
-                R.layout.list_item, // ListItem的XML布局实现
-                new String[] { "ItemTitle", "ItemDetail" },
-                new int[] { R.id.itemTitle, R.id.itemDetail }
-        );
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RateCalcActivity extends AppCompatActivity {
+
+    String TAG = "rateCalc";
+    float rate = 0f;
+    EditText inp2;
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.i(TAG, "onItemLongClick: 长按列表项position=" + position);
+        return true;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_calc);
+        String title = getIntent().getStringExtra("title");
+        rate = getIntent().getFloatExtra("rate",0f);
+
+        Log.i(TAG, "onCreate: title = " + title);
+        Log.i(TAG, "onCreate: rate=" + rate);
+        ((TextView)findViewById(R.id.title2)).setText(title);
+        inp2 = (EditText)findViewById(R.id.inp2);
+        inp2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                TextView show = (TextView) RateCalcActivity.this.findViewById(R.id.show2);
+                if(s.length()>0){
+                    float val = Float.parseFloat(s.toString());
+                    show.setText(val + "RMB==>" + (100/rate*val));
+                }else{
+                    show.setText("");
+                }
+
+            }
+            listItems.remove(position);
+listItemAdapter.notifyDataSetChanged();
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                Log.i(TAG, "onItemLongClick: 长按列表项position=" + position);
+                //删除操作
+                //构造对话框进行确认操作
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("提示").setMessage("请确认是否删除当前数据").setPositiveButton("是",new DialogInterface.OnClickListener(){
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i(TAG, "onClick: 对话框事件处理");
+                        listItems.remove(position);
+                        listItemAdapter.notifyDataSetChanged();
+                    }
+                }).setNegativeButton("否",null);
+                builder.create().show();
+                Log.i(TAG, "onItemLongClick: size=" + listItems.size());
+
+                return true;
+            }
+        });
     }
 }
