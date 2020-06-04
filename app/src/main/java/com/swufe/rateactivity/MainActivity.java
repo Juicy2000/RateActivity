639982a175cdf1ca2688b9d394407f2f8545cc10
Juicy2000 <1076912982@qq.com>
2020-06-04 19:44:51 +0800

    1：汇率计算 2：完成汇率配置页面 3：完成汇率保存 4：使用多线程获取网络数据 5：解析汇率 6：设置汇率更新频率 7：列表的显示 8：自定义列表 9：列表事件处理 10：SQLite数据库

--- app/src/main/java/com/swufe/rateactivity/MainActivity.java
+++ app/src/main/java/com/swufe/rateactivity/MainActivity.java
@@ -451,5 +451,99 @@ public class MainActivity extends AppCompatActivity {
 
         //startActivity(config);
         startActivityForResult(config,1);
+        public class DBHelper extends SQLiteOpenHelper {
 
-    }}
+            private static final int VERSION = 1;
+            private static final String DB_NAME = "myrate.db";
+            public static final String TB_NAME = "tb_rates";
+
+
+            public DBHelper(Context context, String name, CursorFactory factory,
+                            int version) {
+                super(context, name, factory, version);
+            }
+            public DBHelper(Context context) {
+                super(context,DB_NAME,null,VERSION);
+            }
+
+            @Override
+            public void onCreate(SQLiteDatabase db) {
+                db.execSQL("CREATE TABLE "+TB_NAME+"(ID INTEGER PRIMARY KEY AUTOINCREMENT,CURNAME TEXT,CURRATE TEXT)");
+            }
+
+            @Override
+            public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+                // TODO Auto-generated method stub
+            }
+
+        }
+        @Override
+        public void run() {
+            Log.i("List","run...");
+            List<String> retList = new ArrayList<String>();
+            Message msg = handler.obtainMessage();
+            String curDateStr = (new SimpleDateFormat("yyyy-MM-dd")).format(new Date());
+            Log.i("run","curDateStr:" + curDateStr + " logDate:" + logDate);
+            if(curDateStr.equals(logDate)){
+                //如果相等，则不从网络中获取数据
+                Log.i("run","日期相等，从数据库中获取数据");
+                DBManager dbManager = new DBManager(RateListActivity.this);
+                for(RateItem rateItem : dbManager.listAll()){
+                    retList.add(rateItem.getCurName() + "=>" + rateItem.getCurRate());
+                }
+            }else{
+                Log.i("run","日期相等，从网络中获取在线数据");
+                //获取网络数据
+                try {
+                    List<RateItem> rateList = new ArrayList<RateItem>();
+                    URL url = new URL("http://www.usd-cny.com/bankofchina.htm");
+                    HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+                    InputStream in = httpConn.getInputStream();
+                    String retStr = IOUtils.toString(in,"gb2312");
+
+                    //Log.i("WWW","retStr:" + retStr);
+                    //需要对获得的html字串进行解析，提取相应的汇率数据...
+
+                    Document doc = Jsoup.parse(retStr);
+                    Elements tables  = doc.getElementsByTag("table");
+
+                    Element retTable = tables.get(5);
+                    Elements tds = retTable.getElementsByTag("td");
+                    int tdSize = tds.size();
+                    for(int i=0;i<tdSize;i+=8){
+                        Element td1 = tds.get(i);
+                        Element td2 = tds.get(i+5);
+                        //Log.i("www","td:" + td1.text() + "->" + td2.text());
+                        float val = Float.parseFloat(td2.text());
+                        val = 100/val;
+                        retList.add(td1.text() + "->" + val);
+
+                        RateItem rateItem = new RateItem(td1.text(),td2.text());
+                        rateList.add(rateItem);
+                    }
+                    DBManager dbManager = new DBManager(RateListActivity.this);
+                    dbManager.deleteAll();
+                    Log.i("db","删除所有记录");
+                    dbManager.addAll(rateList);
+                    Log.i("db","添加新记录集");
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //更新记录日期
+                SharedPreferences sp = getSharedPreferences("myrate", Context.MODE_PRIVATE);
+                Editor edit = sp.edit();
+                edit.putString(DATE_SP_KEY, curDateStr);
+                edit.commit();
+                Log.i("run","更新日期结束：" + curDateStr);
+            }
+
+            msg.obj = retList;
+            msg.what = msgWhat;
+            handler.sendMessage(msg);
+        }
+
+    }}}
